90cb1f781e5271c5ed1f0196f745c87218ff4648
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2019-06-13 22:59:43 +0200

    added path to reports added parameter 4 categories

--- controllers/wordPressController.js
+++ controllers/wordPressController.js
@@ -9,10 +9,30 @@ exports.getPosts = (req, res, next) => {
     if (req.query.page != undefined) {
         path = path + `&page=${req.query.page}`;
     }
-    const user = request.get(path, {
+
+    if (req.query.category != undefined) {
+        path = path + `&categories=${req.query.category}`;
+    }
+
+    request.get(path, {
         json: true
     }, function (error, response, body) {
         res.json(body);
     });
 
+}
+
+exports.getReports = (req, res, next) => {
+    var hostName;
+    if (req.headers.host.indexOf('localhost') > -1) {
+        hostName = `http://${req.headers.host}`;
+    } else {
+        hostName = `https://${req.headers.host}`;
+    }
+
+    request.get(`${hostName}/api/wp/posts?category=6`, {
+        json: true
+    }, function (error, response, body) {
+        res.json(body);
+    });
 }
--- routes/wordPressRouter.js
+++ routes/wordPressRouter.js
@@ -4,6 +4,7 @@ const wordPressController = require('../controllers/wordPressController');
 const wordPressRouter = express.Router();
 
 wordPressRouter.get('/posts', wordPressController.getPosts);
+wordPressRouter.get('/reports', wordPressController.getReports);
 // teamRouter.get('/:id', wordPressController.getTeam);
 
 module.exports = wordPressRouter;
