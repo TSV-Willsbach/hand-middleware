ab3927d2b2030cccdd6c207a6e7d2a19b8d0d03a
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2019-06-13 21:57:01 +0200

    reverted to mongoose model

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,28 +1,15 @@
 const Team = require('../models/Team');
 
 exports.getTeams = (req, res, next) => {
-    Team.find()
+    Team.find((err, teams) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(teams);
+            }
+        })
         .populate('players')
         .populate('coaches')
-        .exec()
-        .then(docs => {
-            res.status(200).json({
-                count: docs.length,
-                teams: docs.map(doc => {
-                    return {
-                        _id: doc._id,
-                        name: doc.name,
-                        wpCat: doc.wpCat,
-                        players: doc.players,
-                        coaches: doc.coaches,
-                        request: {
-                            type: "GET",
-                            url: "https://api.willsbach-handball.de/api/teams/" + doc._id
-                        }
-                    }
-                })
-            });
-        })
 }
 
 exports.getTeam = (req, res, next) => {
