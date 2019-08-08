ba227cbd8eaa1d519e2eac0a203762ed9d5ee430
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2019-08-08 20:55:51 +0200

    fixed type in wp post Link

--- lib/services/wordPress/wpPostService.ts
+++ lib/services/wordPress/wpPostService.ts
@@ -9,7 +9,7 @@ export class wpPostService extends wordpressService {
     }
 
     public getPosts(page?: number, category?: number, sticky?: boolean) {
-        this.options.uri = this.uri + 'posts/' + `?_embed`;
+        this.options.uri = this.uri + 'posts' + `?_embed`;
 
         if (page != null) {
             this.options.uri = this.options.uri + `&page=${page}`;
